8bb59de20525d4972e89a3f5207717fff515d148
LoonaHelluva <mussakurtseitov@gmail.com>
2024-03-04 23:54:19 +0200

    Created sign up/in activity, created firebase helper class

---
+++ app/src/main/java/com/example/snapz/Classes/FireHelper.kt
@@ -0,0 +1,14 @@
+package com.example.snapz.Classes
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+class FireHelper {
+    companion object{
+        private val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        val Chats = FirebaseDatabase.getInstance().getReference("Chats")
+        val Users = FirebaseDatabase.getInstance().getReference("Users")
+    }
+}

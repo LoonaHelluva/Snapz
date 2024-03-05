ec3252287048a693e92e7a1aa840530e519dcda2
LoonaHelluva <mussakurtseitov@gmail.com>
2024-03-05 12:02:12 +0200

    Added some bug and design fixes into activity_sign_up.xml

--- app/src/main/java/com/example/snapz/SignUp.kt
+++ app/src/main/java/com/example/snapz/SignUp.kt
@@ -26,7 +26,13 @@ class SignUp : AppCompatActivity() {
     lateinit var btSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         //Initializing views
         toSignIn = findViewById(R.id.toSignIn)
         upEmail = findViewById(R.id.upEmail)

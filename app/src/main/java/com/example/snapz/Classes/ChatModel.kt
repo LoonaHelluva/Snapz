be31c81159764fc7cf4bb3329ea44109baf6d7cb
LoonaHelluva <mussakurtseitov@gmail.com>
2024-06-09 23:57:36 +0300

    Update

--- app/src/main/java/com/example/snapz/Classes/ChatModel.kt
+++ app/src/main/java/com/example/snapz/Classes/ChatModel.kt
@@ -3,7 +3,7 @@ package com.example.snapz.Classes
 data class ChatModel(
     var id: String,
     var name: String,
-    var image: String
+    var image: String,
 ){
     constructor() : this("", "", "")
 }
--- app/src/main/java/com/example/snapz/adapters/ChatsAdapter.kt
+++ app/src/main/java/com/example/snapz/adapters/ChatsAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
